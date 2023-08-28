39ba15c8243638c78bd3f350ce8121da90159a8a
Mark Emad Kadies <markemadkadies@gmail.com>
2023-08-28 23:55:26 +0300

    Assignment 1

---
+++ Embedded_C/L1_Assignment/Can/main.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include "can.h"
+
+
+void main(void){
+    can_init();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// typedef struct {
+//     char name[50];
+//     int id;
+// } Employee;
+
+// void (*fun)();
+
+// void print_hello(){
+//     printf("Hello");
+// }
+
+// int main() {
+
+//     Employee e1;
+//     // e1.name = {"Mark Kadies"};
+//     e1 ={"Mark", 15};
+//     printf("%d",e1.id);
+
+
+//    int a;
+//    char *x,*y;
+//    x = (char *)&a;
+//    y = (char *)&a;
+
+//    a = 512;
+//    x[0] = 1;
+//    x[1] = 1;
+// //    *x++;
+//    ++*x;
+
+//    printf("%d\n",a);
+//    return 0;
+
+// }
